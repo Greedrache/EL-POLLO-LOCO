dbff415b57a844c1c94eaade715d4109c799dcb2
Greedrache <greedrache@gmail.com>
2025-12-17 13:50:10 +0100

    .

--- classes/world.class.js
+++ classes/world.class.js
@@ -6,9 +6,16 @@ class World {
         new Chicken()
     ];
 
+    ctx;
 
+    constructor(canvas) {
+        this.ctx = canvas.getContext("2d");
+        this.draw();
+    }
 
     draw() {
+        this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height);
 
+        requestAnimationFrame(() => this.draw());
     }
 }
