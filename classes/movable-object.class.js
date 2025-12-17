11c119ea25853e233333411a521626bbeb0313ea
Greedrache <greedrache@gmail.com>
2025-12-17 13:06:14 +0100

    Erste Classen erstellt

---
+++ classes/movable-object.class.js
@@ -0,0 +1,22 @@
+class MovableObject {
+    x;
+    y;
+    img;
+
+    loadImage(path){
+        this.img = new Image(); 
+        this.img.src = path;
+    }
+
+    moveRight() {
+        console.log("Moving right");
+    }  
+
+    moveLeft() {
+
+    }
+}
+
+
+
+
