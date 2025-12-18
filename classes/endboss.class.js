6a667f0fd9141ae8a3fee2481b8c26febf8d9ef2
Greedrache <greedrache@gmail.com>
2025-12-18 22:21:07 +0100

    map vergrößert

--- classes/endboss.class.js
+++ classes/endboss.class.js
@@ -18,7 +18,7 @@ class Endboss extends MovableObject {
         super();
         this.loadImage(this.IMAGES_WALKING[0]);
         this.loadImages(this.IMAGES_WALKING);
-        this.x = 2750;
+        this.x = 4350;
         this.animate();
     }
 
--- classes/level.class.js
+++ classes/level.class.js
@@ -3,7 +3,7 @@ class Level {
     clouds;
     backgroundObject;
     bottles;
-    level_end_x = 3000;
+    level_end_x = 4400;
 
     constructor(enemies, clouds, backgroundObject, bottles) {
         this.enemies = enemies;
