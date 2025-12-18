0f9987fa4cc92b107b948b90e481baf0f0b87780
Greedrache <greedrache@gmail.com>
2025-12-18 22:47:02 +0100

    JSON für Endboss

--- classes/endboss.class.js
+++ classes/endboss.class.js
@@ -6,7 +6,7 @@ class Endboss extends MovableObject {
     isAlerted = false;
     endboss_music = new Audio('audio/Endboss-Musik.mp3');
     
-    IMAGES_WALKING = [
+    IMAGES_ALERT = [
         "img/4_enemie_boss_chicken/2_alert/G5.png",
         "img/4_enemie_boss_chicken/2_alert/G6.png",
         "img/4_enemie_boss_chicken/2_alert/G7.png",
@@ -17,10 +17,29 @@ class Endboss extends MovableObject {
         "img/4_enemie_boss_chicken/2_alert/G12.png",
     ];
 
+    IMAGES_WALKING = [
+        "img/4_enemie_boss_chicken/1_walk/G1.png",
+        "img/4_enemie_boss_chicken/1_walk/G2.png",
+        "img/4_enemie_boss_chicken/1_walk/G3.png",
+        "img/4_enemie_boss_chicken/1_walk/G4.png",
+    ];
+
+    IMAGES_HURT = [
+        "img/4_enemie_boss_chicken/3_hurt/G21.png",
+        "img/4_enemie_boss_chicken/3_hurt/G22.png",
+        "img/4_enemie_boss_chicken/3_hurt/G23.png",
+    ];
+
+    IMAGES_DEAD = [
+        "img/4_enemie_boss_chicken/4_dead/G24.png",
+        "img/4_enemie_boss_chicken/4_dead/G25.png",
+        "img/4_enemie_boss_chicken/4_dead/G26.png",
+    ];
+
     constructor() {
         super();
-        this.loadImage(this.IMAGES_WALKING[0]);
-        this.loadImages(this.IMAGES_WALKING);
+        this.loadImage(this.IMAGES_ALERT[0]);
+        this.loadImages(this.IMAGES_ALERT);
         this.x = 3800;
         this.endboss_music.loop = true;
         this.endboss_music.volume = 0.5;
@@ -30,7 +49,7 @@ class Endboss extends MovableObject {
     animate() {
         setInterval(() => {
             if (this.isAlerted) {
-                this.playAnimation(this.IMAGES_WALKING);
+                this.playAnimation(this.IMAGES_ALERT);
             }
         }, 200);
     }
