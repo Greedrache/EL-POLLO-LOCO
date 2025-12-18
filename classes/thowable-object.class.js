dc4ffb74362abf93f937efdd524c22cbfba54b13
Greedrache <greedrache@gmail.com>
2025-12-18 20:35:46 +0100

    BOTTLE animation

--- classes/thowable-object.class.js
+++ classes/thowable-object.class.js
@@ -1,25 +1,33 @@
 class ThrowableObject extends MovableObject {
 
+
+    IMAGES_BOTTLE_ROTATE = [
+        "img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png",
+        "img/6_salsa_bottle/bottle_rotation/2_bottle_rotation.png",
+        "img/6_salsa_bottle/bottle_rotation/3_bottle_rotation.png",
+        "img/6_salsa_bottle/bottle_rotation/4_bottle_rotation.png"
+    ];
+
     constructor(x, y) {
         super();
-        this.loadImage("img/6_salsa_bottle/salsa_bottle.png");
+        this.loadImage("img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png");
+        this.loadImages(this.IMAGES_BOTTLE_ROTATE);
         this.x = x;
         this.y = y;
         this.width = 50;
         this.height = 80;
-        this.animate();
         this.trow();
+        this.animate();
     }
 
     animate() {
         setInterval(() => {
-            this.x += 10;
-        }, 25);
+            this.playAnimation(this.IMAGES_BOTTLE_ROTATE);
+        }, 50);
     }
 
     trow() {
         this.speedY = 30;
-        this.speedX = 20;
         this.applyGravity();
         setInterval(() => {
             this.x += 10;
