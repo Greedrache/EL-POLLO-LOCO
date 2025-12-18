026ebb178eb4524a80160d83d6f2c844e6aed27a
Greedrache <greedrache@gmail.com>
2025-12-18 20:28:56 +0100

    Hitboxen werden nicht mehr gezeigt

--- classes/drawable-object.class.js
+++ classes/drawable-object.class.js
@@ -13,13 +13,7 @@ class DrawableObject {
     }
 
     drawFrame(ctx) {
-        if (this instanceof Character || this instanceof Chicken || this instanceof Endboss) {
-            ctx.beginPath();
-            ctx.lineWidth = "1";
-            ctx.strokeStyle = "red";
-            ctx.rect(this.x, this.y, this.width, this.height);
-            ctx.stroke();
-        }
+
     }
 
     draw(ctx) {
